f10194c2ca6f0260ad3ee36835285690de92bc0c
Antonio Mora <antoniocmora@gmail.com>
2012-12-27 00:00:00 +0000

    version 1.00

--- R/summary_protein.R
+++ R/summary_protein.R
@@ -39,17 +39,17 @@ summary_protein = function(id_type, id_value, MITAB_table) {
 		all_interactions_B = MITAB_table[which(MITAB_table$icrogidb == id_value),]
 		all_interactions = unique(rbind(all_interactions_A, all_interactions_B))
 
-		all_degree = length(unique(as.vector(all_interactions$irigid)))
+		all_degree = length(unique(as.vector(all_interactions$icrigid)))
 		interactions_X = all_interactions[which(all_interactions$edgetype == "X"),]
-		number_interactions_X = length(unique(as.vector(interactions_X$irigid)))
+		number_interactions_X = length(unique(as.vector(interactions_X$icrigid)))
 		interactors_X = sort(unique(c(as.vector(interactions_X$icrogida), as.vector(interactions_X$icrogidb))))
 		interactors_X =  interactors_X[which(interactors_X != "-" & interactors_X != id_value)]
 		
 		interaction_this_value_C = all_interactions[which(all_interactions$edgetype == "C"),]
-		irigid_this_C = unique(as.vector(interaction_this_value_C$irigid))
+		irigid_this_C = unique(as.vector(interaction_this_value_C$icrigid))
 		vector_interactors_C = NULL
 		for (i in irigid_this_C) {
-			interactions_C = MITAB_table[which(MITAB_table$irigid == i),]
+			interactions_C = MITAB_table[which(MITAB_table$icrigid == i),]
 			interactors_C = sort(unique(as.vector(interactions_C$icrogidb)))
 			interactors_C =  interactors_C[which(interactors_C != "-" & interactors_C != id_value)]
 			interactors_C = paste(interactors_C, sep="", collapse=",")
@@ -59,7 +59,7 @@ summary_protein = function(id_type, id_value, MITAB_table) {
 		interactors_C = vector_interactors_C
 
 		interactions_Y = all_interactions[which(all_interactions$edgetype == "Y"),]
-		number_interactions_Y = length(unique(as.vector(interactions_Y$irigid)))
+		number_interactions_Y = length(unique(as.vector(interactions_Y$icrigid)))
 		interactors_Y = sort(unique(c(as.vector(interactions_Y$icrogida), as.vector(interactions_Y$icrogidb))))
 		interactors_Y =  interactors_Y[which(interactors_Y != "-" & interactors_Y != id_value)]
 	}
