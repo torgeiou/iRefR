e42d51d287f68c666bd056ab88ca593ca92721f6
torgeiou <torgeirous@hotmail.com>
2018-01-17 17:34:58 +0100

    Update get_irefindex to support version 14.0
    The version "current" not working in iRefR because of different "release_date" value.

--- R/get_irefindex.R
+++ R/get_irefindex.R
@@ -14,8 +14,8 @@ get_irefindex = function(tax_id="All", iref_version="current", data_folder=getwd
 
 	# 2. Get release dates and full URLs:
 	if (iref_version == "current") {
-		iref_version = "13.0"
-		release_date = "08122013"
+		iref_version = "14.0"
+		release_date = "07042015"
 		url = paste("http://irefindex.org/download/irefindex/data/current/psi_mitab/MITAB2.6/", tax_id, ".mitab.", release_date, ".txt.zip", sep="")
 	} else {
 		if (iref_version == "7.0") {
@@ -44,6 +44,10 @@ get_irefindex = function(tax_id="All", iref_version="current", data_folder=getwd
 		}
 		if (iref_version == "13.0") {
 			release_date = "08122013"
+			url = paste("http://irefindex.org/download/irefindex/data/archive/release_", iref_version, "/psi_mitab/MITAB2.6/", tax_id, ".mitab.", release_date, ".txt.zip", sep="")
+		}
+		if (iref_version == "14.0") {
+			release_date = "07042015"
 			url = paste("http://irefindex.org/download/irefindex/data/archive/release_", iref_version, "/psi_mitab/MITAB2.6/", tax_id, ".mitab.", release_date, ".txt.zip", sep="")
 		}
 	}
